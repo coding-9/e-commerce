2beccd92db98733db6639f151cc1a2db756c794e
Ryan <ryanfrynelson@hotmail.fr>
2019-11-27 16:34:42 +0100

    Created redux store
    created a redux store that takes a reducer and the logger middleware

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import {createStore, applyMiddleware} from 'redux';
+import reducer from '../reducer/reducer';
+import logger from 'redux-logger';
+
+const store = createStore(
+    reducer,
+    applyMiddleware(logger)
+);
+
+export default store;
