594fb231d3a88e0a56b7c99f03f955999c3699d4
Hugo Bortolozzi <hugo_bortolozzi@yahoo.fr>
2019-12-04 14:10:17 +0100

    Products Cards

---
+++ src/components/products/ProductList.jsx
@@ -0,0 +1,27 @@
+import React,{ useEffect, useState} from "react"
+import ProductCard from "./ProductCard"
+import {getProductsByGender} from "../../store/products/products"
+
+const ProductList = () =>{
+    
+    const [products, setProducts] = useState([])
+
+    const getProducts = async () => {
+        let array = await getProductsByGender("man")
+        setProducts(array)
+    }
+
+    return(
+        <div className="container">
+            <button onClick={getProducts}>ICI</button>
+                <ul className="list-unstyled productList row">
+                {products.map((product) => {
+                    console.log(product)
+                   return  <li className="col-4"><ProductCard props={product} key={product.id}/></li>
+                })}
+                </ul>
+        </div>
+    )
+}
+
+export default ProductList
