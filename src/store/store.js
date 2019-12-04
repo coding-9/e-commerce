3bf19e4861f3e88c1131e0856b260c09259b76d1
Ryan <ryanfrynelson@hotmail.fr>
2019-12-04 11:32:21 +0100

    Added Persistence
    Changed the redux store to a persist store that stores the previous state

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,21 @@
-import {createStore, applyMiddleware} from 'redux';
-import reducer from '../reducers/productsReducer';
+import {createStore} from 'redux';
+import cart from '../reducers/productsReducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 
-const store = createStore(
-    reducer,
-    applyMiddleware(logger),
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, cart)
+
+
+let store = createStore(
+    persistedReducer,
+    applyMiddleware(logger)
     
 );
-
-export default store;
+let persistor = persistStore(store)
+export { store, persistor }
